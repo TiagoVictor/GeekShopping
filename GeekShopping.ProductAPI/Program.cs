37e2a3dd353e2b5ab04b53b4969b837f8a724e85
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2023-05-23 00:00:34 -0300

    Configurando string de conexão + entidades da classe Product

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+UseMySql(builder.Configuration.GetConnectionString("geek_shopping_product_api"),
+        new MySqlServerVersion(new Version(8, 0, 33))));
 
 var app = builder.Build();
 
