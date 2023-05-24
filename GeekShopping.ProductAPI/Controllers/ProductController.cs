b814f8a817459229e31e7b7fc3c6e7417f7e44ad
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2023-05-23 23:50:18 -0300

    Criando repository + mapper + vo + controller de produtos

---
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FindAll()
+        {
+            var products = await _repository.FindAll();
+
+            return Ok(products);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductVO vo)
+        {
+            if (vo == null)
+                return BadRequest();
+
+            var product = await _repository.Create(vo);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductVO vo)
+        {
+            if (vo == null)
+                return BadRequest();
+
+            var product = await _repository.Update(vo);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+
+            if (!status)
+                return BadRequest();
+
+            return Ok(status);
+        }
+    }
+}
--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,11 +11,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+});
 
 builder.Services.AddDbContext<MySQLContext>(options => options.
 UseMySql(builder.Configuration.GetConnectionString("geek_shopping_product_api"),
         new MySqlServerVersion(new Version(8, 0, 33))));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
