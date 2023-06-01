67c75eedc48489036036a7b4ab2fe2ec1a48be2c
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2023-05-31 23:11:45 -0300

    Criando classe de contexto para o Identity Server + adicionando Packages faltantes

---
+++ GeekShoopping.IdentityServer/Model/Context/MySQLContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShoopping.IdentityServer.Model.Context
+{
+    public class MySQLContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+    }
+}
